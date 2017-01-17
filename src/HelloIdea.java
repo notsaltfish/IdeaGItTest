5a0a2e233a073b1a266043d02d2448a7f27cf56a
notsaltedfish <c1097210504@qq.com>
2017-01-17 19:26:04 +0800

    继续测试git

--- src/HelloIdea.java
+++ src/HelloIdea.java
@@ -4,6 +4,12 @@ public class HelloIdea {
 
         System.out.println("HelloIdeaGit");
         System.out.println();
+
+        System.out.println("你好");
         System.out.println("dsdsds");
+
     }
+
+
+
 }
