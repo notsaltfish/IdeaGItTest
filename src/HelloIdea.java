8defdec9e54302c7dba2f9102ab2a9456cece30d
notsaltedfish <c1097210504@qq.com>
2017-01-17 19:53:41 +0800

    继续测试git

--- src/HelloIdea.java
+++ src/HelloIdea.java
@@ -1,7 +1,8 @@
 
 public class HelloIdea {
     public static void main(String[] args){
-
+        String str = new String("sdsds");
+        System.out.println(str);
         System.out.println("HelloIdeaGit");
         System.out.println();
 
