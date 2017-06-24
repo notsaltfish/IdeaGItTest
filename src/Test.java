0fd8fa6b59d9b54f7344078f58b8b33a8178c6dc
chenyong <c1097210504@qq.com>
2017-06-25 00:29:07 +0800

    添加Test.java

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by chenyong on 2017-06-24 23:25.
+ *在桌面git bash添加 Test.java文件 测试与idea pull下这个文件时当中已经存在的
+ *Test.java但是没有添加到git版本仓库的文件是否会冲突
+ */
+public class Test {
+   
+   public void testGit(){
+	   
+   }
+}
